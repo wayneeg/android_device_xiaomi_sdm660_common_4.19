2585c05a63b4b8b24acc75b4318d48dc2b5daf84
Marc Bourgoin <themard85@gmail.com>
2021-09-16 13:05:48 +0300

    sdm660-common: Drop QCOM WFD

--- sdm660.mk
+++ sdm660.mk
@@ -604,15 +604,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libaacwrapper \
-    libnl
-
-# WiFi Display JAR
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # DeviceSettings
 PRODUCT_PACKAGES += \
    DeviceSettings
